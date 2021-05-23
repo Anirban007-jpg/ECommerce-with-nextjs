ad722de78d18547fc8b0cfbc66bb5cd9d79f9df2
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-23 21:13:05 +0530

    server setup complete. Closes #1

---
+++ Backend/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cp = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//app
+const app = express();
+
+// middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cp())
+
+// db
+// mongoose.connect(parameter1 = connection string in .env file using dotenv package, {if any warning include it here}).then(() => {logic});
+mongoose.connect(process.env.DATABASE,{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true,useFindAndModify:false}).then(() => {console.log('database connected');})
+
+//cors
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors({origin: `${process.env.CLIENT_URL}`}));    
+}
+
+
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError'){
+        res.status(401).json({
+            error: "Unauthorized!"
+        });
+    }
+});
+
+
+// handle port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
