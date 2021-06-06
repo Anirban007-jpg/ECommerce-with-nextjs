735c446f971cc5b3dc530e994fb53cf94a981a2c
Anirban Banerjee <abanerjee763@outlook.com>
2021-06-06 20:03:25 +0530

    Closes #17

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,10 +1,33 @@
 import Layout  from "../components/Layout"
+import React from "react"
+  
+import Head from 'next/head';
+import { API_NAME, DOMAIN } from "../config";
 
 const HomePage = () => {
+
+    const head = () => (
+        <Head>
+            <title>Home Page | {API_NAME}</title>
+            <meta name="description"/>
+            <link rel="canonical" href={`${DOMAIN}`} />
+            <meta property="og:title" content={`Home Page | ${API_NAME}`} />
+            <meta name="og:description" content="This is the landing Page" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}`} />
+            <meta property="og:site_name" content={`${API_NAME}`} />
+            <meta property="og:image:secure_url" />
+            <meta property="og:image:type" content={"image/jpg"} />
+        </Head>
+    )
+
     return (
-        <Layout>
-            Welcome to Amazonia! Home Page yet to be designed
-        </Layout>
+        <React.Fragment>
+            {head()}
+            <Layout>
+                Welcome to Amazonia! Home Page yet to be designed
+            </Layout>
+        </React.Fragment>
     )
 }
   
--- Frontend/pages/register.js
+++ Frontend/pages/register.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import RegisterComponent from '../components/auth/RegisterComponent';
 import Layout from '../components/Layout';
+  
+import Head from 'next/head';
+import { API_NAME, DOMAIN } from "../config";
 
 const register = () => {
+
+    const head = () => (
+        <Head>
+            <title>Register Page | {API_NAME}</title>
+            <meta name="description"/>
+            <link rel="canonical" href={`${DOMAIN}/register`} />
+            <meta property="og:title" content={`Register Page | ${API_NAME}`} />
+            <meta name="og:description" content="This is the Registration Page" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}`} />
+            <meta property="og:site_name" content={`${API_NAME}`} />
+            <meta property="og:image:secure_url" />
+            <meta property="og:image:type" content={"image/jpg"} />
+        </Head>
+    )
+
     return (
-        <Layout>
-            <React.Fragment>
-                <br />
-                <h2 className="text-center">Registration Form</h2><br/>
-                <div className="mr-3 ml-3 pr-3 pl-3">
-                    <RegisterComponent />
-                </div>
-            </React.Fragment>
-        </Layout>
+        <React.Fragment>
+            {head()}
+            <Layout>
+                    <br />
+                    <h2 className="text-center">Registration Form</h2><br/>
+                    <div className="mr-3 ml-3 pr-3 pl-3">
+                        <RegisterComponent />
+                    </div>
+            </Layout>
+        </React.Fragment>
     )
 }
 
